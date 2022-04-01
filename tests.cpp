c7c812fac3b4d595d32d9431aec95a0f7b7e23c7
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2022-03-31 22:31:28 -0400

    Task B testing

--- tests.cpp
+++ tests.cpp
@@ -18,3 +18,20 @@ TEST_CASE("Length of vectors"){
   points = {1, 4.0/18.0, 11.0/15.0};
   CHECK(length(&points) == doctest::Approx(1.25982558072).epsilon(0.02));
 }
+
+TEST_CASE("Farthest vector from origin"){
+  Coord3D secondVector = {1, 2, 3};
+  Coord3D firstVector = {15, 4, 5};
+
+  CHECK(fartherFromOrigin(&secondVector, &firstVector) == &firstVector);
+
+  firstVector = {7, 8, 14};
+  secondVector = {6.33, 7.33, 8.14};
+
+  CHECK(fartherFromOrigin(&secondVector, &firstVector) == &firstVector);
+
+  firstVector = {21, 1, 1};
+  secondVector = {11, 15, 0};
+
+  CHECK(fartherFromOrigin(&secondVector, &firstVector) == &firstVector);
+}
